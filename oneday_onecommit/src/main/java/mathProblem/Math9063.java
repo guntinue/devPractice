ea588aa5eb1fb2d8a8bb28017e0943cb8d8ea3cc
guntinue <rjssn93@gmail.com>
2025-11-23 23:59:53 +0900

    11/23 commit

---
+++ oneday_onecommit/src/main/java/mathProblem/Math9063.java
@@ -0,0 +1,32 @@
+package mathProblem;
+
+import java.util.Scanner;
+
+public class Math9063 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int pNum = sc.nextInt();
+
+//        int x = sc.nextInt();
+//        int y = sc.nextInt();
+
+        int result = 0;
+
+        for (int i = 1; i <= pNum; i++) {
+
+            if (pNum == 1) {
+                System.out.println(result);
+            } else {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+
+                int area =
+
+                System.out.println(area);
+            }
+        }
+    }
+}
