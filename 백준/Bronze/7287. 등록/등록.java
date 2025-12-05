1ac28fa42b691d4bf0aaa5e109acd9988eae57d4
keonwoo lee <rjssn93@gmail.com>
2025-12-05 23:16:41 +0900

    [Bronze V] Title: 등록, Time: 104 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int cnum = 11;
+        String id = "guntinue";
+        
+        System.out.println(cnum);
+        System.out.println(id);
+        
+    }
+}
