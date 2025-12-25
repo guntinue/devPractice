7e925f4eae1f1dc7319ce68f423fed5781c0f422
guntinue <rjssn93@gmail.com>
2025-12-25 23:17:25 +0900

    12/25 commit merry x-mas

--- oneday_onecommit/src/main/java/solved/sprout/sprout11654.java
+++ oneday_onecommit/src/main/java/solved/sprout/sprout11654.java
@@ -10,15 +10,13 @@ public class sprout11654 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 2. 한 줄을 읽어온 뒤, 그 줄의 첫 번째 문자(charAt(0))를 가져옵니다.
-        char input = br.readLine().charAt(0);
+        char n = br.readLine().charAt(0);
 
-        // 3. char 타입을 int 타입으로 강제 형 변환(Casting)합니다.
-        // 자바에서 문자는 내부적으로 아스키/유니코드 숫자로 저장되어 있습니다.
-        int asciiValue = (int) input;
+        // char 타입을 int 타입으로 강제 형 변환
+        int asciiCode = (int) n;
 
-        // 4. 아스키 코드 값을 출력합니다.
-        System.out.println(asciiValue);
+        // 아스키 코드 값 출력
+        System.out.println(asciiCode);
 
         br.close();
     }
