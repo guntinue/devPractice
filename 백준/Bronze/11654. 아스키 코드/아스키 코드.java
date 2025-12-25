7faa12a7c6585bc4e7bea26b8ae52c28aa711b9b
keonwoo lee <rjssn93@gmail.com>
2025-12-25 23:15:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char n = br.readLine().charAt(0);
+
+        int asciiCode = (int) n;
+
+        System.out.println(asciiCode);
+
+        br.close();
+    }
+}
