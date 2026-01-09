92015eaa1701b2b6e5aa285059a616868d20334d
keonwoo lee <rjssn93@gmail.com>
2026-01-10 00:01:26 +0900

    [Bronze V] Title: 문자열, Time: 96 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+        int T = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < T; i++) {
+           
+            String str = br.readLine();
+            
+            char first = str.charAt(0);
+            char last = str.charAt(str.length() - 1);
+            
+            sb.append(first).append(last).append("\n");
+        }
+        
+        System.out.print(sb);
+        
+    }
+}
