fdf30e097c50f5a37335e4fa9b7ad68508d55b4c
keonwoo lee <rjssn93@gmail.com>
2026-01-13 00:14:36 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 5548 ms, Memory: 406640 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+        
+        System.out.println(n.multiply(m));
+    }
+}
